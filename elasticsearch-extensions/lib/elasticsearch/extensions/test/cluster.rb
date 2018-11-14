8615cbfe0411fa4088feef07d2dfe95096b597df
Alex Lopez <webodessa@gmail.com>
2018-11-14 12:22:06 -0500

    Update cluster.rb

--- elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
+++ elasticsearch-extensions/lib/elasticsearch/extensions/test/cluster.rb
@@ -456,7 +456,7 @@ module Elasticsearch
               arguments[:version]
             elsif File.exist?(path_to_lib) && !(jar = Dir.entries(path_to_lib).select { |f| f =~ /^elasticsearch\-\d/ }.first).nil?
               __log "Determining version from [#{jar}]" if ENV['DEBUG']
-              if m = jar.match(/elasticsearch-cli\-(\d+\.\d+\.\d+).*/)
+              if m = jar.match(/elasticsearch(-?.*?)\-(\d+\.\d+\.\d+).*/)
                 m[1]
               else
                 raise RuntimeError, "Cannot determine Elasticsearch version from jar [#{jar}]"
